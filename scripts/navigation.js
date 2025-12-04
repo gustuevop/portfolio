95bd0776e24819bbec934763aa69e2d060c5fe16
Gustavo Prado <gustavo.spro@outlook.com>
2025-12-03 22:44:41 -0300

    Projects occupying all screen and script remove

--- scripts/navigation.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", () => {
-  const nav = document.querySelector(".navigation");
-
-  if (window.scrollY > window.innerHeight) {
-    nav.classList.add("scrolled");
-  } else {
-    nav.classList.remove("scrolled");
-  }
-});
